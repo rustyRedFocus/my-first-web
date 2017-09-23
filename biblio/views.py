d2e06198c3521ca996e42eac274d9e0a7190b35a
ford <ford@ff.com>
2017-09-23 16:35:58 +0200

    Zmieniony kod html i plik views.py

--- biblio/views.py
+++ biblio/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Book
 
 
 def books_list(request):
-	return render(request, 'biblio/books_list.html', {})
+	books = Book.objects.filter(borrow_date__lte=timezone.now()).order_by('borrow_date')
+	return render(request, 'biblio/books_list.html', {'books': books})
