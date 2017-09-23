a80a3baf3f98fdc52dacfab13d595cfecebe3d4d
ford <ford@ff.com>
2017-09-23 16:48:20 +0200

    Próba aktualizacji wyświetlania modeli z bazy

--- biblio/views.py
+++ biblio/views.py
@@ -4,5 +4,6 @@ from .models import Book
 
 
 def books_list(request):
-	books = Book.objects.filter(borrow_date__lte=timezone.now()).order_by('borrow_date')
+	#books = Book.objects.filter(borrow_date__lte=timezone.now()).order_by('borrow_date')
+	books = Book.objects.all()
 	return render(request, 'biblio/books_list.html', {'books': books})
