2677c45f705180ce7c791cab242319421c9e4016
ford <ford@ff.com>
2017-10-15 18:30:19 +0200

    Dodano mozliwosc edycji i dodawania wpisow

--- biblio/urls.py
+++ biblio/urls.py
@@ -2,7 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.books_list, name='books_list'),
+	url(r'^$', views.borrow_video, name='borrow_video'),
+	url(r'^video/(?P<pk>[0-9]+)/$', views.video_detail, name='video_detail'),
+	url(r'^video/new/$', views.video_new, name='video_new'),
+	url(r'^video/(?P<pk>[0-9]+)/edit/$', views.video_edit, name='video_edit'),
+	
 
 
 ]
--- biblio/views.py
+++ biblio/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Book, User
+from .models import Book, User, Video
+from .forms import VideoForm
 
 
 def books_list(request):
@@ -10,3 +11,42 @@ def books_list(request):
 
 
 	return render(request, 'biblio/books_list.html', {'books': books, 'users' : users})
+
+def video_detail(request, pk):
+	video = get_object_or_404(Video, pk=pk)
+	return render(request, 'biblio/video_detail.html', {'video' : video})
+
+def video_new(request):
+
+	if request.method == "POST":
+		form = VideoForm(request.POST)
+		if form.is_valid():
+			video = form.save(commit=False)
+		#post.author = request.user
+		#post.published_date = timezone.now()
+			video.save()
+			return redirect('video_detail', pk=video.pk)
+	else:
+		form = VideoForm()
+	return render(request, 'biblio/video_edit.html', {'form' : form})
+
+
+def video_edit(request, pk):
+    video = get_object_or_404(Video, pk=pk)
+    if request.method == "POST":
+        form = VideoForm(request.POST, instance=video)
+        if form.is_valid():
+            video = form.save(commit=False)
+            #post.author = request.user
+            #post.published_date = timezone.now()
+            video.save()
+            return redirect('video_detail', pk=video.pk)
+    else:
+        form = VideoForm(instance=video)
+    return render(request, 'biblio/video_edit.html', {'form': form})
+
+def borrow_video(request):
+	videos = Video.objects.order_by('title')
+
+	return render(request, 'biblio/videos_list.html', {'videos' : videos})
+
